95567ba803b07766a22bff7d2d59ea0ba434f79b
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-30 08:04:30 -0400

    placeholder

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,13 +2,13 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
+    const needed_funding = document.querySelector('#day-of-week').value.trim();
     const description = document.querySelector('#project-desc').value.trim();
   
     if (name && needed_funding && description) {
       const response = await fetch(`/api/projects`, {
         method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
+        body: JSON.stringify({ name, day_of_week: selectedDay, description }), // Update field name to 'day_of_week'
         headers: {
           'Content-Type': 'application/json',
         },
