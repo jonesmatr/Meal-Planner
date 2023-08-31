3e1c7a29683c875022feb8ce57a309da7afe632f
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-31 07:46:04 -0400

    Updated profile page.

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -2,19 +2,49 @@ const router = require('express').Router();
 const { Project } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// router.post('/', withAuth, async (req, res) => {
+//   console.log(req.body);
+//   try {
+//     const newProject = await Project.create({
+//       ...req.body,
+//       user_id: req.session.user_id,
+//     });
+
+//     res.status(200).json(newProject);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
 router.post('/', withAuth, async (req, res) => {
   console.log(req.body);
   try {
+    const {
+      name,
+      needed_funding,
+      description,
+      meal_name,
+      day_of_week,
+      recipe,
+    } = req.body;
+
     const newProject = await Project.create({
-      ...req.body,
+      name,
+      needed_funding,
+      description,
+      meal_name,
+      day_of_week,
+      recipe,
       user_id: req.session.user_id,
     });
 
     res.status(200).json(newProject);
   } catch (err) {
+    console.error(err);
     res.status(400).json(err);
   }
 });
+
 
 
 
