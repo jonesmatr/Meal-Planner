357a09847024be6e32580f83c6af02e17e030a1e
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-09-02 07:30:36 -0400

    Added API

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const cors = require('cors');
 
 
 const sequelize = require('./config/connection');
