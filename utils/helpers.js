5af70b74d39ebaca890964a31f4cdf245f4f8301
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-09-05 19:40:15 -0400

    Added more emojies

--- utils/helpers.js
+++ utils/helpers.js
@@ -13,7 +13,11 @@ module.exports = {
       // Return a random emoji
       if (randomNum > 0.7) {
         return `<span for="img" aria-label="lightbulb">🌮</span>`;
-      } else if (randomNum > 0.4) {
+      } else if (randomNum > 0.6) {
+        return `<span for="img" aria-label="laptop">🍕</span>`;
+      } else if (randomNum > 0.4){
+        return `<span for="img" aria-label="laptop">🍣</span>`;
+      } else if (randomNum > 0.2){
         return `<span for="img" aria-label="laptop">🍔</span>`;
       } else {
         return `<span for="img" aria-label="gear">🍗</span>`;
